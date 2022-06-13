6cc947a457e3036c1bfcf24108d21190d49a4c3f
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-13 14:47:46 -0300

    Se le cambio la manera de mostrar.

--- JS/buttons.js
+++ JS/buttons.js
@@ -1,11 +1,11 @@
 let menu = document.getElementById("prueba");
-let see = document.getElementById("navDisplay")
+let see = document.getElementById("navDisplay");
 
 let displayMenu = () =>{ 
     new Promise((resolve, reject) => {
         if (see.style.clipPath != "inset(0px)") resolve(see.style.clipPath = "inset(0 0 0 0)");
         else reject();
-    }).catch(()=> see.style.clipPath = null)
+    }).catch(()=> see.style.clipPath = null);
     
 }
 
